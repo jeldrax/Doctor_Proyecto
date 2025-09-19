d6504145bdbcf64540ada19530e77f961c10aa1d
pablo <pabloe080404@mal.com>
2025-09-18 18:49:53 -0600

    layout_taildwind

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/','/admin');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::middleware([
     'auth:sanctum',
